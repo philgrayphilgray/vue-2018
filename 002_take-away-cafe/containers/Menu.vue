d48866a3c1a3fe79b1dcc8ca8f3a1b8c102152f0
philgrayphilgray <thephilgray@gmail.com>
2018-04-13 09:37:49 -0400

    Create basic grid layout for product grid.

--- 002_take-away-cafe/containers/Menu.vue
+++ 002_take-away-cafe/containers/Menu.vue
@@ -1,4 +1,33 @@
 <template lang="pug">
-.MenuWrapper
-    h2 Menu
+.menu
+    .menu__message
+        p   Choose a menu item below to view more details and make a selection.
+    .menu__item(v-for="n in 4") {{n }}
+
 </template>
+
+<style lang="scss">
+@import '../theme.scss';
+.menu {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-areas: 'message message';
+  background: $primary-dark;
+  grid-gap: $spacing-unit;
+  margin: $spacing-unit/2;
+}
+
+.menu__message {
+  grid-area: message;
+  background: $primary;
+  padding: $spacing-unit;
+  text-align: center;
+}
+
+.menu__item {
+  text-align: center;
+  box-shadow: 1px 2px 4px 1px $secondary-light;
+}
+</style>
+
